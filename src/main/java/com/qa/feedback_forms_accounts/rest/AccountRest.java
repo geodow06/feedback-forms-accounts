ec52a7433252a1b98c134f0964416b445a2412b6
fortunexfortune <osadolor.fortune@yahoo.it>
2019-02-18 18:15:04 +0000

    completed crud implementation

--- src/main/java/com/qa/feedback_forms_accounts/rest/AccountRest.java
+++ src/main/java/com/qa/feedback_forms_accounts/rest/AccountRest.java
@@ -3,9 +3,13 @@ package com.qa.feedback_forms_accounts.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +40,22 @@ public class AccountRest {
 //        sendToQueue(account);
     	return service.addAccount(account);
     }
+    
+    @GetMapping("${path.getAccountById}")
+    public Account getAccount(@PathVariable Long id) {
+        return service.getAccount(id);
+    }
+
+    @DeleteMapping("${path.deleteAccount}")
+    public ResponseEntity<Object> deleteAccount(@PathVariable Long id) {
+        return service.deleteAccount(id);
+    }
+
+    @PutMapping("${path.updateAccount}")
+    public ResponseEntity<Object> updateAccount(@RequestBody Account account, @PathVariable Long id) {
+        return service.updateAccount(account, id);
+    }
+    
     
     
 
