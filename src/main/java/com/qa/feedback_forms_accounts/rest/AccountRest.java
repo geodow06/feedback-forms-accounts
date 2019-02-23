db8c639c29063528ddba53b51d0db7978a22aebc
fortunexfortune <osadolor.fortune@yahoo.it>
2019-02-23 01:23:40 +0000

    removed requests expect for Create method and added email validation for Qa emails that works for Mongo

--- src/main/java/com/qa/feedback_forms_accounts/rest/AccountRest.java
+++ src/main/java/com/qa/feedback_forms_accounts/rest/AccountRest.java
@@ -1,16 +1,10 @@
 package com.qa.feedback_forms_accounts.rest;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,43 +23,18 @@ public class AccountRest {
     
     @Autowired
     private JmsTemplate jmsTemplate;
-	
-    @GetMapping("${path.getAccounts}")
-    public List<Account> getAccounts() {
-        return service.getAccounts();
-    }
      
     @PostMapping("${path.createAccount}")
-    public Account createAccount(@RequestBody Account account) {
-    	SentAccount accountToSend = new SentAccount(account);
-    	sendToQueue(accountToSend);
-        
-        
+    public Account createAccount(@RequestBody Account account) {    	
+		if(account.getEmail().endsWith("@qa.com") || account.getEmail().endsWith("@academytrainee.com")) {
+	    	SentAccount accountToSend = new SentAccount(account);
+			sendToQueue(accountToSend);
+		}
     	return service.addAccount(account);
     }
-    
-    @GetMapping("${path.getAccountById}")
-    public Account getAccount(@PathVariable Long id) {
-        return service.getAccount(id);
-    }
-
-    @DeleteMapping("${path.deleteAccount}")
-    public Account deleteAccount(@PathVariable Long id) {
-        return service.deleteAccount(id);
-    }
-
-    @PutMapping("${path.updateAccount}")
-    public Account updateAccount(@RequestBody Account account, @PathVariable Long id) {
-        return service.updateAccount(account, id);
-    }
-    
     
     private void sendToQueue(SentAccount sentAccount){
         jmsTemplate.convertAndSend("AccountQueue", sentAccount);
     }
     
-    
-
-
-
 }
--- src/main/java/com/qa/feedback_forms_accounts/service/AccountServiceImpl.java
+++ src/main/java/com/qa/feedback_forms_accounts/service/AccountServiceImpl.java
@@ -1,32 +1,17 @@
 package com.qa.feedback_forms_accounts.service;
 
-import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qa.feedback_forms_accounts.persistence.domain.Account;
 import com.qa.feedback_forms_accounts.persistence.repository.AccountRepository;
-import com.qa.feedback_forms_accounts.util.exceptions.AccountNotFoundException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
 	@Autowired
 	private AccountRepository repo;
-
-	@Override
-	public List<Account> getAccounts() {
-		return repo.findAll();
-	}
 	
 	@Override
 	public Account addAccount(Account account) {
@@ -35,35 +20,5 @@ public class AccountServiceImpl implements AccountService {
 		}
 		return null;
 	}
-
-
-	@Override
-	public Account getAccount(Long id) {
-		Optional<Account> account = repo.findById(id);
-		return account.orElseThrow(() -> new AccountNotFoundException(id));
-	}
-
-	@Override
-	public Account deleteAccount(Long id) {
-		Optional<Account> account = repo.findById(id);
-		if (accountExists (id)) {
-			repo.deleteById(id);
-		}
-		return account.orElseThrow(() -> new AccountNotFoundException(id));
-	}
-
-	@Override
-	public Account updateAccount(Account account, Long id) {
-		if (accountExists(id)) {
-			account.setAccountID(id);
-			repo.save(account);
-		}
-		Optional<Account> accountInDB = repo.findById(id);
-		return accountInDB.orElseThrow(() -> new AccountNotFoundException(id));
-	}
-
-	private boolean accountExists(Long id) {
-		Optional<Account> accountOptional = repo.findById(id);
-		return accountOptional.isPresent();
-	}
+	
 }
