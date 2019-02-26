dc2103bcc78270438099f90cc3c02a69773c2a34
imallinson <imallinson1001@gmail.com>
2019-02-26 10:24:07 +0000

    fix ternary operator

--- src/main/java/com/qa/feedback_forms_accounts/rest/AccountRest.java
+++ src/main/java/com/qa/feedback_forms_accounts/rest/AccountRest.java
@@ -2,6 +2,7 @@ package com.qa.feedback_forms_accounts.rest;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,12 +24,18 @@ public class AccountRest {
     
     @Autowired
     private JmsTemplate jmsTemplate;
+    
+    @Value("${trainer.email}")
+    private String trainerEmail;
+    
+    @Value("${trainee.email}")
+    private String traineeEmail;
      
     @PostMapping("${path.createAccount}")
     public Account createAccount(@RequestBody Account account) {    	
-		if(account.getEmail().endsWith("@qa.com") || account.getEmail().endsWith("@academytrainee.com")) {
-			account.getEmail().endsWith("@qa.com") ? account.setAdmin(true) : account.setAdmin(false);			
-	    		SentAccount accountToSend = new SentAccount(account);
+		if(account.getEmail().endsWith(trainerEmail) || account.getEmail().endsWith(traineeEmail)) {
+			account.setAdmin(account.getEmail().endsWith(trainerEmail) ? true : false);			
+	    	SentAccount accountToSend = new SentAccount(account);
 			sendToQueue(accountToSend);
 		}
     	return service.addAccount(account);
