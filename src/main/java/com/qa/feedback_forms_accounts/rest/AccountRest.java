b66977c62e30f527c03fc3660f43b8b8ffdcc7f7
fortunexfortune <osadolor.fortune@yahoo.it>
2019-02-27 12:14:09 +0000

    added more functions to  the create account response

--- src/main/java/com/qa/feedback_forms_accounts/rest/AccountRest.java
+++ src/main/java/com/qa/feedback_forms_accounts/rest/AccountRest.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.qa.feedback_forms_accounts.persistence.domain.Account;
 import com.qa.feedback_forms_accounts.persistence.domain.SentAccount;
@@ -17,31 +18,49 @@ import com.qa.feedback_forms_accounts.service.AccountService;
 @RequestMapping("${path.base}")
 @RestController
 public class AccountRest {
-	
-    @Autowired
-    private AccountService service;
-    
-    @Autowired
-    private JmsTemplate jmsTemplate;
-    
-    @Value("${trainer.email}")
-    private String trainerEmail;
-    
-    @Value("${trainee.email}")
-    private String traineeEmail;
-     
-    @PostMapping("${path.createAccount}")
-    public Account createAccount(@RequestBody Account account) {    	
-		if(account.getEmail().endsWith(trainerEmail) || account.getEmail().endsWith(traineeEmail)) {
-			account.setAdmin(account.getEmail().endsWith(trainerEmail) ? true : false);			
-	    	SentAccount accountToSend = new SentAccount(account);
-			sendToQueue(accountToSend);
+
+	@Autowired
+	private AccountService service;
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${trainer.email}")
+	private String trainerEmail;
+
+	@Value("${trainee.email}")
+	private String traineeEmail;
+
+	@Value("${path.genAccountByEmail}")
+	private String genAccountByEmail;
+
+	@Value("${url.retriever}")
+	private String retrieverURL;
+
+	@PostMapping("${path.createAccount}")
+	public Account createAccount(@RequestBody Account account) {
+		if (account.getEmail().endsWith(trainerEmail) || account.getEmail().endsWith(traineeEmail)) {
+			account.setAdmin(account.getEmail().endsWith(trainerEmail) ? true : false);
+			Account accountinDB = requestGetAccountByEmail(account.getEmail());
+			if (accountinDB == null) {
+				SentAccount accountToSend = new SentAccount(account);
+				sendToQueue(accountToSend);
+				return account;
+			}
 		}
-    	return service.addAccount(account);
-    }
-    
-    private void sendToQueue(SentAccount sentAccount){
-        jmsTemplate.convertAndSend("AccountQueue", sentAccount);
-    }
-    
+		return null;
+	}
+
+	private Account requestGetAccountByEmail(String email) {
+		Account response = restTemplate.getForObject(retrieverURL + genAccountByEmail + email, Account.class);
+		return response;
+	}
+
+	private void sendToQueue(SentAccount sentAccount) {
+		jmsTemplate.convertAndSend("AccountQueue", sentAccount);
+	}
+
 }
